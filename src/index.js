e7c07601c090be6a3598ba2b1a2591a6fabfd48c
EnjoyableProgramming <63019353+EnjoyableProgramming@users.noreply.github.com>
2020-09-22 15:36:24 -0500

    React Menu First Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import ContainerComponent from './Components/Container/Container.jsx'
+
+ReactDOM.render(<ContainerComponent></ContainerComponent>, document.getElementById('root'));
